7b514d303c54703ee26be2f6f6a68e36e368ab70
abhinavrajeshh <abhinav.x50@gmail.com>
2026-03-19 18:21:28 +0530

    Starting the odin project

---
+++ Javascript/practice/script.js
@@ -0,0 +1,16 @@
+// let name = "Abhinav";
+// let size = name.length;
+// console.log(name);
+// console.log(size)
+// console.log(name[name.length - 2])
+// --------------------------------
+// let statement = "She said 'Hello'";
+// console.log(statement)
+
+// -------------------------
+// console.log(statement)
+// var statement = "She said \"Hello\" ";
+
+// -------------------------
+
+
--- README.md
+++ README.md
@@ -2,11 +2,4 @@
 
 >This repository tracks my journey to becoming a MERN stack developer.
 
-Goal : 
-
-- Build strong fundamentals in HTML, CSS, and JavaScript
-- Learn backend with Node & Express
-- Work with MongoDB
-- Build and deploy full-stack projects
-
 > Rule: Consistent progress. No restarting.
