6ab32682c319c3b5f5d4af1999bf8a9c6e6d357c
abhinavrajeshh <abhinav.x50@gmail.com>
2026-04-25 08:30:23 +0530

    Restarted learning

---
+++ theOdinproject/4. JS/Assignments/1.js
@@ -0,0 +1,15 @@
+// let num1 = 23;
+// let num2 = 97;
+// let sum = num1 + num2;
+// console.log(sum);
+// -----------------
+let n1, n2 , n3 , n4, n5, n6;
+n1 = 10;
+n2 = 20;
+n3 = 30;
+n4 = 40;
+n5 = 50;
+n6 = 60;
+
+let sum = n1 + n2 + n3 + n4 + n5 + n6;
+console.log(sum)
