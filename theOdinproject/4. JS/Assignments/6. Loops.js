a1770976b7f3602900ce1ea533da5f5342a53e3b
abhinavrajeshh <abhinav.x50@gmail.com>
2026-04-28 12:47:21 +0530

    practicing

---
+++ theOdinproject/4. JS/Assignments/6. Loops.js
@@ -0,0 +1,17 @@
+// -------LOOPS------
+
+// for(let i = 1; i<=5;i++){
+// console.log("*");
+// }
+// -------------
+// let i = 1;
+// while(i<=5){
+// console.log(i);
+// i++
+// }
+// ----------
+// let i = 1;
+// do {
+// console.log("*");
+// i++;
+// } while(i<= 5);
