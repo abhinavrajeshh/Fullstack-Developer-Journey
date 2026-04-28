9bc7c2537485d82e685f1db75764dc6dae2bc237
abhinavrajeshh <abhinav.x50@gmail.com>
2026-04-28 12:47:59 +0530

    going to nested loop

--- theOdinproject/4. JS/Assignments/6. Loops.js
+++ theOdinproject/4. JS/Assignments/6. Loops.js
@@ -15,3 +15,17 @@
 // console.log("*");
 // i++;
 // } while(i<= 5);
+// -------------
+// practice section--------
+
+// let i;
+// for(i = 1; i<=10;i++){
+// console.log(i);
+// }
+// ------------------
+// for(let i = 1; i<=20;i++){
+// if(i%2==0){
+// console.log(i);
+
+// }
+// }
