8eb81f034e4609f92b6918b87f05a533cb2c9a0f
abhinavrajeshh <abhinav.x50@gmail.com>
2026-04-29 13:39:19 +0530

    doing star exercises

--- theOdinproject/4. JS/Assignments/6. Loops.js
+++ theOdinproject/4. JS/Assignments/6. Loops.js
@@ -191,15 +191,29 @@
 //   console.log(row);
 // }
 // -----------------------
-let n = 5;
-for(let i = 1;i<=n ; i++){
-    let row = "";
-    for(let j = 1; j<= n - i; j++){
-        row= row + "#";
-    }
-    for(let j = 1; j<=i;j++){
-        row = row + "* ";
-    }
-    console.log(row);
+// let n = 5;
+// for(let i = 1;i<=n ; i++){
+//     let row = "";
+//     for(let j = 1; j<= n - i; j++){
+//         row= row + "#";
+//     }
+//     for(let j = 1; j<=i;j++){
+//         row = row + "* ";
+//     }
+//     console.log(row);
     
-}
+// }
+// /-------------------
+
+// let n = 5;
+// for (let i = 1; i<=n; i++){
+//     let row = "";
+//     for(let j = 1; j<=n-i; j++){
+//         row+="  ";
+//     }
+//     for(let k =1; k<=2*i-1;k++){
+//         row+="* "
+//     }
+//     console.log(row);
+    
+// }
