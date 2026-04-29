5eafad1f045f269d5bcbdc0279e55151f777acdf
abhinavrajeshh <abhinav.x50@gmail.com>
2026-04-29 13:25:38 +0530

    doing star exercises

--- theOdinproject/4. JS/Assignments/6. Loops.js
+++ theOdinproject/4. JS/Assignments/6. Loops.js
@@ -165,3 +165,28 @@
 
 // ------------------
 
+// for(let i = 1; i<= 5; i++){
+//     let row = ""
+//     for(let j = i; j<=5; j++){
+//         row += "* ";
+//     }
+//     console.log(row);
+// }
+// --------------------
+// let n = 5;
+
+// for (let i = 1; i <= n; i++) {
+//   let row = "";
+
+//   // spaces
+//   for (let j = 1; j <= n - i; j++) {
+//     row += " ";
+//   }
+
+//   // stars
+//   for (let j = 1; j <= i; j++) {
+//     row += "* ";
+//   }
+
+//   console.log(row);
+// }
