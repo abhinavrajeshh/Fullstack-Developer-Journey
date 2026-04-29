db42da8e85558333f510930156434070a5627a75
abhinavrajeshh <abhinav.x50@gmail.com>
2026-04-29 13:56:07 +0530

    learning star pattern printing using loops

--- theOdinproject/4. JS/Assignments/6. Loops.js
+++ theOdinproject/4. JS/Assignments/6. Loops.js
@@ -218,3 +218,16 @@
     
 // }
 // ---------------------------
+
+let n = 5;
+for (let i = 1; i<= n ; i++){
+     let row = "";
+    for(let j= n-1; j>=1; j--){
+        row = row + " "
+    }
+    for(let k = i; k>=1; k--){
+        row = row + "* "
+    }
+    console.log(row);
+    
+}
