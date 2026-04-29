2d939f408c1169761aa3b5e08399b78ddaa61aab
abhinavrajeshh <abhinav.x50@gmail.com>
2026-04-29 14:57:08 +0530

    going through functions

---
+++ theOdinproject/2. JS/3. Functions/1.js
@@ -0,0 +1,48 @@
+// function sayHello() {
+//     console.log("Hello");
+// }
+// sayHello();
+// ---------------------
+// function add(a, b){
+//     return a + b;
+// }
+// let result = add(5, 3);
+// console.log(result);
+
+
+
+// let result = add (34, 89);
+// console.log(result);
+
+// -------------------------
+// practice---
+
+// function hello(){
+//     console.log("Hello gpt");
+    
+// }
+// hello();
+// ---------------
+// function numtake(num){
+//     return num*num;
+// }
+// let square = numtake(5)
+// console.log(square);
+//  --------------
+// function bignumfinder(n1, n2){
+//     return n1>n2? n1:n2;
+// }
+// console.log(bignumfinder(50, 99));
+
+// ----------------
+
+// function greet(name){
+//     return "Hello "+ name;
+// }
+// console.log(greet("GPT"));
+// -------------
+function biggest(num1, num2, num3){
+    let max = num1>num2?num1:num2;
+    return max > num3?max:num3;
+}
+console.log(biggest(102, 203, 209));
