723fd53062b0988fb144eeb21f124fbb99dbbe83
abhinavrajeshh <abhinav.x50@gmail.com>
2026-05-02 14:29:08 +0530

    next chapter of odin, loops and arrays exercises

--- theOdinproject/2. JS/4. Rock Paper Scissors/Practice/1.js
+++ theOdinproject/2. JS/4. Rock Paper Scissors/Practice/1.js
@@ -44,15 +44,28 @@
 
 // console.log(Math.floor(Math.random()*3));
 // -------------
-function getComputerChoice(){
-  let num = Math.floor(Math.random()*3);
-  if(num===0){
-    return "rock";
-  }else if(num===1){
-    return "paper";
-  }else{
-    return "scissors"
+// function getComputerChoice(){
+//   let num = Math.floor(Math.random()*3);
+//   if(num===0){
+//     return "rock";
+//   }else if(num===1){
+//     return "paper";
+//   }else{
+//     return "scissors"
+//   }
+// }
+// console.log(getComputerChoice());
+// ----------------------
+// let arr = [1,2,3,4,5];
+// for(let i = 0;i<arr.length;i++){
+//   console.log(arr[i]);
+// }
+// /--------------------
+let arr = [3,10,5];
+let max = arr[0];
+for(let i=1;i<arr.length;i++){
+  if(max<arr[i]){
+   max=arr[i]
   }
 }
-console.log(getComputerChoice());
-
+console.log(max);
