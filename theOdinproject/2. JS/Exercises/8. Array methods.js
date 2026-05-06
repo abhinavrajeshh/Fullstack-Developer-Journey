ee86804e08f8d6474a9f9ffe72d4af766c022789
abhinavrajeshh <abhinav.x50@gmail.com>
2026-05-06 12:01:36 +0530

    practicing map method

---
+++ theOdinproject/2. JS/Exercises/8. Array methods.js
@@ -0,0 +1,20 @@
+// let arr = [1, 2, 3, 4, 5];
+// let result = arr.map(num => num*2);
+// console.log(result);
+// console.log(arr);
+// -----------------------
+// function doubleArray(arr){
+//   let arr2 = [];
+//   for(let i = 0; i<arr.length; i++){
+//     arr2.push(arr[i] * 2);
+//   }
+//   return arr2;
+// }
+// console.log(doubleArray([1,2,3,4,5]));
+// --CONVERT THIS TO MAP METHOD-----
+
+function doubleArray(arr){
+    let arr2 = arr.map(num => num*2);
+    return arr2;
+}
+console.log(doubleArray([1,2,3,4,5]));
