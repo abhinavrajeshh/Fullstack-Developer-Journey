4188cb83954784fb65c766d7c7c5682aaaf655c4
abhinavrajeshh <abhinav.x50@gmail.com>
2026-05-07 16:21:58 +0530

    did one more

--- theOdinproject/2. JS/Exercises/8. Array methods.js
+++ theOdinproject/2. JS/Exercises/8. Array methods.js
@@ -114,3 +114,17 @@
 // }
 // console.log(removefromArray([1,2,3,4], 3));
 // ----------------------------
+function sumAll(num1, num2) {
+    let sum = 0;
+
+    let start = num1 < num2 ? num1 : num2;
+    let end = num1 > num2 ? num1 : num2;
+
+    for(let i = start; i <= end; i++){
+        sum = sum + i;
+    }
+
+    return sum;
+}
+
+console.log(sumAll(4, 1));
--- theOdinproject/2. JS/Exercises/tempCodeRunnerFile.js
+++ theOdinproject/2. JS/Exercises/tempCodeRunnerFile.js
@@ -1,5 +1,1 @@
-function longWords(arr){
-    let long = arr.filter(a => a.length>4);
-    return long;
-}
-console.log(longWords(["cat", "elephant", "dog", "tiger"]));
+let sum = 0;
