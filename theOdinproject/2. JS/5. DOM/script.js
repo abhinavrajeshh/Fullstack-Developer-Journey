f041610f8c3463c6062d4eb4c1750fc3f156dc47
abhinavrajeshh <abhinav.x50@gmail.com>
2026-05-11 14:09:38 +0530

    getting started with DOM

--- theOdinproject/2. JS/5. DOM/script.js
+++ theOdinproject/2. JS/5. DOM/script.js
@@ -1,2 +1,8 @@
-let title = document.querySelector("#title");
-title.textContent = "Hello Abhinav";
+// let title = document.querySelector("#title");
+// title.textContent = "Hello Abhinav";
+
+let title = document.querySelector('#title');
+title.textContent = "changed sub";
+
+let newLe = document.querySelector("#h3");
+newLe.textContent = "this is changed now"
