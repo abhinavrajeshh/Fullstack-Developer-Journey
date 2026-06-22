ccc3ff384cfb6888972516ae742d0e21b18ed7d8
abhinavrajeshh <abhinav.x50@gmail.com>
2026-06-22 13:26:27 +0530

    practing

--- theOdinproject/3. JS/practice.js
+++ theOdinproject/3. JS/practice.js
@@ -8,3 +8,60 @@
 // }else{
 //     console.log("Minor");
 // }
+
+// // Round 3
+// for(let i = 1; i<=5; i++){
+//     console.log(i);
+// }
+// // Round 4
+// function add(a,b){
+//     return a+b;
+// }
+// let num1 = 10;
+// let num2 = 20;
+// let sum =  add(num1, num2);
+// console.log("Sum : ",sum);
+// ---------------
+
+// // Round 5
+
+// let arr = ["apple", "banana", "orange"];
+// let accessingBanana = arr[1];
+// console.log(accessingBanana);
+
+// // Round 6
+
+// let obj = {
+//     name : "John",
+//     age : 30,
+//     place : "London"
+// }
+// console.log("age : ",obj.age);
+// obj.place = "Thailand";
+
+// console.log(obj.place);
+
+// // Round 7 -------
+// let arr = [1,2,3,4,5];
+// let newArr = [];
+// for(let i = 0; i<=arr.length-1; i++){
+//     if(arr[i]>3){
+//         newArr.push(arr[i])
+//     }
+// }
+// console.log(newArr);
+
+// ------------------
+// Array methods
+// let fruits = [ "apple", "banana"];
+// fruits.push("orange");
+// console.log(fruits);
+
+// let fruits = ["apple", "banana", "orange"];
+// fruits.pop();
+// console.log(fruits);
+// ----------
+let numbers = [2,3,4];
+
+numbers.unshift(1);
+console.log(numbers);
