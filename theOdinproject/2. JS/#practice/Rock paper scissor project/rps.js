86cc64e5bf7b3af5e3a7f294faf45195c4d374ce
abhinavrajeshh <abhinav.x50@gmail.com>
2026-08-04 12:25:53 +0530

    on progress

--- theOdinproject/2. JS/#practice/Rock paper scissor project/rps.js
+++ theOdinproject/2. JS/#practice/Rock paper scissor project/rps.js
@@ -1,10 +1,49 @@
-// console.log("Hello");
+// Practice-------
+//  console.log("Hello");
 
 // function animal(name){
 //     return name + " is good";
 // }
 // console.log(animal("goat"));
 
-let num = Math.random()*3;
-console.log(num)
-console.log(Math.floor(num));
+// ---------------------------------
+//------------- LOGIC---------
+// let num = Math.random()*3;
+// console.log(num)
+// console.log(Math.floor(num));
+// ---------------------------------
+
+function getComputerChoice(){
+    let a = Math.random()*3;
+    let value = Math.floor(a);
+    console.log(value);
+    
+    if(value===0){
+        return "rock";
+    }else if(value===1){
+        return "paper";
+    }else{
+        return "scissors";
+    }
+}
+let computer = getComputerChoice();
+console.log(computer);
+
+function getHumanChoice(){
+    let value = prompt("rock , paper or scissors? ");
+    return value;
+}
+let human = getHumanChoice();
+console.log(human);
+
+function playround(human, computer){
+    if(human==="rock" && computer ==="paper" || human === "paper" && computer === "scissors" || human === "scissors" && computer === "rock"){
+        
+    }   
+    else if(human === "rock" && computer === "scissors" || human === "paper" && computer === "rock" || human === "scissors" && computer === "paper") 
+        return "Human win"; 
+    else{
+        return "Draw"
+    }
+}
+console.log(playround());
