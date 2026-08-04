dac58d5f5c604dd05a6b9d3adecb79de7179a4e7
abhinavrajeshh <abhinav.x50@gmail.com>
2026-08-04 11:15:43 +0530

    remembering the logic

---
+++ theOdinproject/2. JS/#practice/Rock paper scissor project/rps.js
@@ -0,0 +1,10 @@
+// console.log("Hello");
+
+// function animal(name){
+//     return name + " is good";
+// }
+// console.log(animal("goat"));
+
+let num = Math.random()*3;
+console.log(num)
+console.log(Math.floor(num));
---
+++ theOdinproject/2. JS/#practice/Rock paper scissor project/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+console.log("Hello");
+
+// function animal(name){
+//     return name + " is good";
+// }
+// console.log(animal("goat"));
